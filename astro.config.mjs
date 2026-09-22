8f76f1389a3a506dcf283327114821932589f626
slowcreate <selmalouise00@gmail.com>
2026-09-22 14:05:20 +0200

    connecting react and adding database structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
   },
